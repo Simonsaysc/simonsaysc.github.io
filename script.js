1238b2841418ac72a79538f3abf18705cbc74689
Simón Alonso <131623500+Simonsaysc@users.noreply.github.com>
2025-03-19 17:23:59 -0300

    News

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const apiKey = "f1d02c8f1bb643c7af37bb441189b3aa"; 
-const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${apiKey}`
+const apiKey = "f1d02c8f1bb643c7af37bb441189b3aa"; // Replace this with your ACTUAL API key!
+const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${apiKey}`;
 
 async function fetchAndDisplayNews() {
   try {
@@ -16,40 +16,39 @@ async function fetchAndDisplayNews() {
   }
 }
 
-
 function displayNews(articles) {
-    const newsContainer = document.getElementById("news-container");
-    if(newsContainer){
-      newsContainer.innerHTML = '';
-    
-    articles.forEach(article=> {
-      const col = document.createElement('div');
-      col.classList.add('col-md-4', 'mb-4');
+  const newsContainer = document.getElementById("news-container");
+  if (newsContainer) {
+    newsContainer.innerHTML = "";
 
-      const articleElement = document.createElement('div');
-      articleElement.classList.add('article', 'card', 'h-100');
+    articles.forEach((article) => {
+      const col = document.createElement("div");
+      col.classList.add("col-md-4", "mb-4");
 
-      const title = document.createElement('h5');
+      const articleElement = document.createElement("div");
+      articleElement.classList.add("article", "card", "h-100");
+
+      const title = document.createElement("h5");
       title.textContent = article.title;
-      title.classList.add('card-title');
+      title.classList.add("card-title");
       articleElement.appendChild(title);
 
-      const description = document.createElement('p');
-      description.textContent = article.description;
-      description.classList.add('card-text');
+      const description = document.createElement("p");
+      description.textContent = article.description || "No description available.";
+      description.classList.add("card-text");
       articleElement.appendChild(description);
 
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = article.url;
-      link.textContent = 'Read more';
-      link.target = '_blank'; 
-      link.classList.add('btn', 'btn-primary'); 
+      link.textContent = "Read more";
+      link.target = "_blank";
+      link.classList.add("btn", "btn-primary");
       articleElement.appendChild(link);
 
       col.appendChild(articleElement);
       newsContainer.appendChild(col);
     });
-};
+  }
 }
 addEventListener("DOMContentLoaded", () => {
   fetchAndDisplayNews();
